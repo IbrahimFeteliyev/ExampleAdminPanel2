2657b0bbafac42d47f26f8abd5e05704e4aa61dd
İbrahim Fətəliyev <ibrahim.fataliyev.c@asoiu.edu.az>
2022-02-12 21:30:29 +0400

    12.02.2022

---
+++ Example/Areas/admin/Controllers/PortfoliosController.cs
@@ -0,0 +1,33 @@
+﻿using Example.Areas.admin.ViewModel;
+using Example.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Example.Areas.admin.Controllers
+{
+    public class PortfoliosController
+    {
+        [Area("admin")]
+        public class PortfoliosController : Controller
+        {
+            private readonly ExampleDbContext _context;
+
+            public PortfoliosController(ExampleDbContext context)
+            {
+                _context = context;
+
+            }
+
+
+            public IActionResult Index()
+            {
+
+                AdminVM vm = new()
+                {
+                    Portfolios = _context.Portfolios.ToList()
+                };
+                return View(vm);
+            }
+        }
+    }
+}
---
+++ Example/Areas/admin/ViewModel/AdminVM.cs
@@ -0,0 +1,10 @@
+﻿using Example.Models;
+
+namespace Example.Areas.admin.ViewModel
+{
+    public class AdminVM
+    {
+        public List<Portfolio> Portfolios { get; set; }
+
+    }
+}
